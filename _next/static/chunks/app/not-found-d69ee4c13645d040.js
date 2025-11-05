d3e17da798ed4a1076ca9e3837ea82bdeb6fec36
vracton <vracto@icloud.com>
2025-11-04 22:50:16 -0600

    intro & study

---
+++ _next/static/chunks/app/not-found-d69ee4c13645d040.js
@@ -0,0 +1,23 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[345],{11024:(e,u,t)=>{"use strict";t.r(u),t.d(u,{default:()=>n});var r=t(35695),s=t(12115);let n=()=>{let{replace:e}=(0,r.useRouter)();return(0,s.useEffect)(()=>e("/"),[e]),null}},25030:(e,u,t)=>{Promise.resolve().then(t.bind(t,11024))},35695:(e,u,t)=>{"use strict";var r=t(18999);t.o(r,"usePathname")&&t.d(u,{usePathname:function(){return r.usePathname}}),t.o(r,"useRouter")&&t.d(u,{useRouter:function(){return r.useRouter}})}},e=>{var u=u=>e(e.s=u);e.O(0,[441,684,358],()=>u(25030)),_N_E=e.O()}]);
+
+
+/*
+     FILE ARCHIVED ON 08:38:04 Jun 14, 2025 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 04:24:43 Nov 05, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.48
+  exclusion.robots: 0.019
+  exclusion.robots.policy: 0.009
+  esindex: 0.009
+  cdx.remote: 5.407
+  LoadShardBlock: 124.9 (3)
+  PetaboxLoader3.datanode: 233.519 (5)
+  load_resource: 354.197 (2)
+  PetaboxLoader3.resolve: 200.164 (2)
+*/
---
+++ _next/static/chunks/main-app-c29131b30cea0236.js
@@ -0,0 +1,23 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[358],{19393:()=>{},59288:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,90894,23)),Promise.resolve().then(n.t.bind(n,94970,23)),Promise.resolve().then(n.t.bind(n,26614,23)),Promise.resolve().then(n.t.bind(n,46975,23)),Promise.resolve().then(n.t.bind(n,87555,23)),Promise.resolve().then(n.t.bind(n,74911,23)),Promise.resolve().then(n.t.bind(n,59665,23)),Promise.resolve().then(n.t.bind(n,31295,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[441,684],()=>(s(35415),s(59288))),_N_E=e.O()}]);
+
+
+/*
+     FILE ARCHIVED ON 08:38:04 Jun 14, 2025 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 04:24:43 Nov 05, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.529
+  exclusion.robots: 0.021
+  exclusion.robots.policy: 0.011
+  esindex: 0.01
+  cdx.remote: 23.367
+  LoadShardBlock: 171.938 (3)
+  PetaboxLoader3.datanode: 106.375 (5)
+  PetaboxLoader3.resolve: 194.192 (4)
+  load_resource: 721.428 (2)
+*/
