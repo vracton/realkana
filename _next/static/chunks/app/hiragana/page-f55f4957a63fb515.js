c3280a1d4d61d01eed0c5127fc3d2cd61cfaf5b7
vracton <vracto@icloud.com>
2025-11-04 23:00:39 -0600

    hiragana

---
+++ _next/static/chunks/app/hiragana/page-f55f4957a63fb515.js
@@ -0,0 +1,24 @@
+
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[332],{},_=>{var e=e=>_(_.s=e);_.O(0,[163,359,477,483,613,347,572,441,684,358],()=>e(49572)),_N_E=_.O()}]);
+
+
+/*
+     FILE ARCHIVED ON 08:38:04 Jun 14, 2025 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 04:51:02 Nov 05, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.498
+  exclusion.robots: 0.017
+  exclusion.robots.policy: 0.007
+  esindex: 0.009
+  cdx.remote: 12.532
+  LoadShardBlock: 150.444 (3)
+  PetaboxLoader3.datanode: 133.733 (4)
+  load_resource: 130.798
+  PetaboxLoader3.resolve: 83.685
+*/
