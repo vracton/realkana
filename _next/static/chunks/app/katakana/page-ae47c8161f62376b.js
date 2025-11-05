f4aa84b998a1632a24113b97b6bd920569eb4034
vracton <vracto@icloud.com>
2025-11-04 23:11:23 -0600

    katakana

---
+++ _next/static/chunks/app/katakana/page-ae47c8161f62376b.js
@@ -0,0 +1,24 @@
+
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[161],{},_=>{var e=e=>_(_.s=e);_.O(0,[163,359,477,483,613,347,572,441,684,358],()=>e(49572)),_N_E=_.O()}]);
+
+
+/*
+     FILE ARCHIVED ON 08:38:05 Jun 14, 2025 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 05:00:18 Nov 05, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.598
+  exclusion.robots: 0.022
+  exclusion.robots.policy: 0.011
+  esindex: 0.01
+  cdx.remote: 35.723
+  LoadShardBlock: 98.214 (3)
+  PetaboxLoader3.datanode: 97.039 (4)
+  load_resource: 85.899
+  PetaboxLoader3.resolve: 56.181
+*/
